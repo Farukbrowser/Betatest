5386dea7e835a8e9b2fc29deba1b5f986f448528
Farukbrowser <40906618+Farukbrowser@users.noreply.github.com>
2022-06-29 01:43:43 -0700

    Add files via upload

---
+++ cekconfig.sh
@@ -0,0 +1,16 @@
+# !/bin/bash
+# cekconfig = melihat daftar config
+
+IP=`curl ipv4.icanhazip.com`
+echo -e "  List Config Yang Tersedia" | lolcat
+echo -e "============================="| lolcat
+cd /var/www/openvpn
+ls
+echo -e " Semua Config" | lolcat
+echo -e "http://$IP:85/configs.zip" | lolcat
+echo -e "http://$IP:85/index.html" | lolcat
+echo -e "=============================" | lolcat
+echo -e "  Config .ehi (HTTP Injector) " | lolcat
+echo -e "  Config .ovpn (OpenVPN)" | lolcat
+echo -e "(OpenVPN HTTP Custom(Open Cfg VPN))" | lolcat
+echo -e "  Mod by Faruk Browser"  | lolcat
